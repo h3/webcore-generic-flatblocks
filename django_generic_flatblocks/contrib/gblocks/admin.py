25b7a512d926a0df4c4bc37f61e3d5d410c7775c
Maxime Haineault <max@motion-m.ca>
2012-03-01 00:25:59 -0500

    Added GBLOCK_GRPAPPELLI_TINYMCE option

--- django_generic_flatblocks/contrib/gblocks/admin.py
+++ django_generic_flatblocks/contrib/gblocks/admin.py
@@ -2,26 +2,31 @@ from django.conf import settings
 from django.contrib import admin
 from django_generic_flatblocks.contrib.gblocks.models import *
 
+if getattr(settings, 'GBLOCK_GRPAPPELLI_TINYMCE', False):
+    GBLOCK_JS = [
+        '%sgrappelli/tinymce/jscripts/tiny_mce/tiny_mce.js' % settings.STATIC_URL,
+        '%swebsite/js/tinymce_setup.js' % settings.STATIC_URL]
+else:
+    GBLOCK_JS = []
 
 if 'grappellifit' in settings.INSTALLED_APPS and 'modeltranslation' in settings.INSTALLED_APPS:
     from grappellifit.admin import TranslationAdmin
-    
-    admin.site.register(Title, TranslationAdmin)
-    admin.site.register(Text, TranslationAdmin)
-    admin.site.register(Image)
-    admin.site.register(ImageAndLink, TranslationAdmin)
-    admin.site.register(TitleAndFile, TranslationAdmin)
-    admin.site.register(TitleTextAndFile, TranslationAdmin)
-    admin.site.register(TitleAndText, TranslationAdmin)
-    admin.site.register(TitleTextAndImage, TranslationAdmin)
 
+    class GblockAdmin(TranslationAdmin):
+        class Media:
+            js = GBLOCK_JS
 else:
-    admin.site.register(Title)
-    admin.site.register(Text)
-    admin.site.register(Image)
-    admin.site.register(ImageAndLink)
-    admin.site.register(TitleAndFile)
-    admin.site.register(TitleTextAndFile)
-    admin.site.register(TitleAndText)
-    admin.site.register(TitleTextAndImage)
 
+    class GblockAdmin(admin.ModelAdmin):
+        class Media:
+            js = GBLOCK_JS
+    
+
+admin.site.register(Title, GblockAdmin)
+admin.site.register(Text, GblockAdmin)
+admin.site.register(Image)
+admin.site.register(ImageAndLink, GblockAdmin)
+admin.site.register(TitleAndFile, GblockAdmin)
+admin.site.register(TitleTextAndFile, GblockAdmin)
+admin.site.register(TitleAndText, GblockAdmin)
+admin.site.register(TitleTextAndImage, GblockAdmin)
